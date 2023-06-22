831f1ca711c93e0e914a284ce0f2800069775020
Ramiro Volker <ramiro.volker@hotmail.com>
2023-06-22 18:04:57 -0300

    cambio muy leve en el js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ checkboxes = Array.from(checkboxes);
 console.log(checkboxes);
 
 checkboxes.forEach((box) => {
-    box.addEventListener("click",(box1)=>{
-        console.log(box1);
+    box.addEventListener("click",()=>{
+        console.log(box.parentElement);
     })
 });
  
