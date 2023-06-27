d5e9e9f7ece98fbbedbb49ac15ea46db00bc9501
Ramiro Volker <ramiro.volker@hotmail.com>
2023-06-27 17:43:31 -0300

    Arreglado varios errores creando y finalizando tareas, empecé a codificar la funcion para unfinishear tareas

--- script.js
+++ script.js
@@ -29,13 +29,15 @@ taskIcon2 = document.createElement("I");
 taskTemp.appendChild(taskIcon2);
 taskIcon2.classList.add("fa-solid","fa-trash-can","trash-can");
 
-taskIcon2.addEventListener("click",()=>{
-        taskIcon2.parentElement.remove();
+taskIcon2.addEventListener("click",(e)=>{
+        e.target.parentElement.remove();
     });
 
- taskIcon1.addEventListener("click",()=>{
-        finishTask(taskIcon1.parentElement.textContent);
-        taskIcon1.parentElement.remove();
+ taskIcon1.addEventListener("click",(e)=>{
+        console.log(e.target);
+        console.log(e.target.parentElement.textContent);
+        finishTask(e.target.parentElement.textContent);
+        e.target.parentElement.remove();
     })
 }
 
@@ -44,9 +46,6 @@ taskIcon2.addEventListener("click",()=>{
 
 function finishTask(tarea){
     tarea = tarea.trim();
-    if(tarea == ""){
-        return;
-    }
 
 checkeds = document.querySelector(".checkeds");
 
@@ -109,10 +108,54 @@ function checkboxsEL(){
     trashCans = document.querySelectorAll(".checkbox");
     trashCans.forEach((icon)=>{
         icon.addEventListener("click",()=>{
+            console.log(icon.parentElement.textContent);
             finishTask(icon.parentElement.textContent);
             icon.parentElement.remove();
         })
     });
     }
 checkboxsEL();
+/////////////////////////////////////////////////////////////////////
+
+function checkedsEL(){
+    checkedBoxes = document.querySelectorAll(".checkedbox");
+    checkedBoxes.forEach((icon)=>{
+        icon.addEventListener("click",()=>{
+            console.log(icon.parentElement.textContent);
+            unfinishTask(icon.parentElement.textContent);
+            icon.parentElement.remove();
+        })
+    });
+    }
+checkedsEL();
+
+/////////////////////////////////////////////////////////////////////
+// DEVOLVER TAREA
+
+function unfinishTask(tarea){
+    tarea = tarea.trim();
+
+checkeds = document.querySelector(".tasks");
+
+taskTemp = document.createElement("DIV");
+checkeds.prepend(taskTemp);
+taskTemp.classList.add("checked-container");
+
+taskIcon11 = document.createElement("I");
+taskTemp.appendChild(taskIcon11);
+taskIcon11.classList.add("fa-regular","fa-circle","checkbox");
+
+taskPara = document.createElement("P");
+taskTemp.appendChild(taskPara);
+taskPara.innerHTML = tarea;
+taskPara.classList.add("checked");
+
+taskIcon22 = document.createElement("I");
+taskTemp.appendChild(taskIcon22);
+taskIcon22.classList.add("fa-solid","fa-trash-can","trash-can");
+
+trashCansEL();
+}
+
+
 /////////////////////////////////////////////////////////////////////
